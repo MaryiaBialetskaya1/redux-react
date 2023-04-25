a0b12980927d6dda58dac4728d1de3f093dcabae
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-04-25 17:43:18 -0400

    add basic info

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//1 - store -all data
+//2 - action
+
+//A
+const addToCart = () => {
+  return {
+    type: "ADD_TO_CART",
+  };
+};
+//B
+const addItem = () => {
+  return {
+    type: "REMOVE_ITEM",
+  };
+};
+//C
+const changeNumberOFItems = () => {
+  return {
+    type: "CHANGE_NUMBER",
+  };
+};
+
+//3 - reducer
+//4 - dispatch
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
