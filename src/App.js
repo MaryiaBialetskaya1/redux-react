4a4a83cd63d6f70660cc5f41af8c8eea005acd10
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-05-03 16:06:19 -0400

    add feature to add amount by using input box

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import "./App.css";
 import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement } from "./redux-toolkit/counter";
+import {
+  increment,
+  decrement,
+  incrementByAmount,
+} from "./redux-toolkit/counter";
+import { useState } from "react";
 
 function App() {
   const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
+  const [incrementAmount, setIncrementAmount] = useState("2");
 
   return (
     <div className="App">
       <p>Score: {count}</p>
       <button onClick={() => dispatch(increment())}>+</button>
       <button onClick={() => dispatch(decrement())}>-</button>
+      <div>
+        <input
+          aria-label="Set increment amount"
+          value={incrementAmount}
+          onChange={(e) => setIncrementAmount(e.target.value)}
+        />
+        <button
+          onClick={() =>
+            dispatch(incrementByAmount(Number(incrementAmount) || 0))
+          }
+        >
+          Add Amount
+        </button>
+      </div>
     </div>
   );
 }
--- src/redux-toolkit/counter.js
+++ src/redux-toolkit/counter.js
@@ -14,10 +14,15 @@ export const counterSlice = createSlice({
     decrement: (state) => {
       state.value -= 1;
     },
+    incrementByAmount: (state, action) => {
+      state.value += action.payload;
+    },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { increment, decrement } = counterSlice.actions;
+export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+
+export const selectCount = (state) => state.counter.value;
 
 export default counterSlice.reducer;
